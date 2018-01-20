c63ef36ba6a6a557b0a8be8df04f133335122157
Stella Coumbassa <stella.coumbassa@rwth-aachen.de>
2018-01-20 09:31:21 +0100

    changing the json format + writing to json file

--- aufgabe_a1.py
+++ aufgabe_a1.py
@@ -5,18 +5,22 @@ import json
 
 
 with open("outfile","rb") as f:
-    l = pickle.load(f)
+	l = pickle.load(f)
 
 #l = [[1,2,3],[2,3,4],[4,5,6]]
 l = MyAlgo.findGroups(l[0])
 list = list()
 for s in l:
-    #print(s._elements)
-    #print(s._count)
-    string_rep = str(s._elements) + ", " + str(s._count)
-    #print(string_rep)
-    list.append(string_rep)
+	#print(s._elements)
+	#print(s._count)
+	string_rep=""
+	for tag_id in s._elements:
+		string_rep += tag_id + ": " + str(s._count) + ", "
+
+	list.append(string_rep)
 
 
 json_string = json.dumps(list)
 print(json_string)
+with open('data.json', 'w') as outfile:
+	json.dump(list, outfile)
