93e9f4a2863ddf0018e428d53569ced35bf538a9
Stella Coumbassa <stella.coumbassa@rwth-aachen.de>
2018-01-20 09:05:34 +0100

    converting to json

--- aufgabe_a1.py
+++ aufgabe_a1.py
@@ -1,6 +1,7 @@
 from count_set import Set
 import pickle
 import MyAlgo
+import json
 
 
 with open("outfile","rb") as f:
@@ -8,6 +9,14 @@ with open("outfile","rb") as f:
 
 #l = [[1,2,3],[2,3,4],[4,5,6]]
 l = MyAlgo.findGroups(l[0])
+list = list()
 for s in l:
-    print(s._elements)
-    print(s._count)
+    #print(s._elements)
+    #print(s._count)
+    string_rep = str(s._elements) + ", " + str(s._count)
+    #print(string_rep)
+    list.append(string_rep)
+
+
+json_string = json.dumps(list)
+print(json_string)
