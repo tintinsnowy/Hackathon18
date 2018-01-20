be038922b640236849ede598e107a3f4c6fac33d
Stella Coumbassa <stella.coumbassa@rwth-aachen.de>
2018-01-20 10:28:08 +0100

    json format update

--- aufgabe_a1.py
+++ aufgabe_a1.py
@@ -9,18 +9,29 @@ with open("outfile","rb") as f:
 
 #l = [[1,2,3],[2,3,4],[4,5,6]]
 l = MyAlgo.findGroups(l[0])
-list = list()
+elements = list()
+group = 1
 for s in l:
 	#print(s._elements)
 	#print(s._count)
-	string_rep=""
-	for tag_id in s._elements:
-		string_rep += tag_id + ": " + str(s._count) + ", "
+	data={}
 
-	list.append(string_rep)
+	data["group"] = group
+	data["count"] = s._count
+	data["Id"] = list(s._elements)
+
+	elements.append(data)
+	group += 1
 
 
-json_string = json.dumps(list)
+json_string = json.dumps(elements)
 print(json_string)
 with open('data.json', 'w') as outfile:
-	json.dump(list, outfile)
+	json.dump(elements, outfile)
+
+
+
+#[
+#    {"group":"1","count":"19","Id":["57e4f8","ca3e169b9"]},
+#    {"group":"2","count":"3","Id":["57e4f8sdsd","ca3e169sdsdb9"]}
+#]
