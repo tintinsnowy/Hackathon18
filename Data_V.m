49d81ce19d9a16a1aaa83c0df7a9f88ae10ddc53
Sherry Yang <yangxiaoli1994@gmail.com>
2018-01-20 10:18:20 +0100

    update the Datainput

--- Data_V.m
+++ Data_V.m
@@ -1,2 +1,5 @@
 %conn = mongo('localhost',27017,'RFID')
-[data,str] = xlsread('EPC_Hashes.xlsx', 'A:E');
+[data,str] = xlsread('.\EPC_Hashes.xlsx', 'A:E');
+scatter3(data(:,2),data(:,3),data(:,4))
+strJS = fileread('input.json');
+jsonG = jsondecode(strJS);
